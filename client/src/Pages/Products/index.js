f80c5c5812e24b193359f96e810947e13cbc513b
fatmadurak <fatmabadillidurak@gmail.com>
2023-02-02 14:47:49 +0300

    commit

--- client/src/Pages/Products/index.js
+++ client/src/Pages/Products/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import Card from '../../components/Card'
 import { Grid} from '@chakra-ui/react'
+import {useQuery}from "react-query"
 
 function Products() {
+
+  const { isLoading, error, data } = useQuery('repoData', () =>
+  fetch('https://api.github.com/repos/tannerlinsley/react-query').then(res =>
+    res.json()
+  )
+)
+
+if (isLoading) return 'Loading...'
+
+if (error) return 'An error has occurred: ' + error.message
+
+console.log("data",data)
   return (
     <div>
 
