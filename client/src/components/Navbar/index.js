fb2bfdf56346cc035e7ca6ba871eb183d3b6ade8
fatmadurak <fatmabadillidurak@gmail.com>
2023-02-02 11:58:57 +0300

    commit

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Styles from "./styles.module.css"
 
 function Navbar() {
   return (
-    <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-    </ul>
+    <nav className={Styles.nav}>
+    <div className={Styles.left}>
+       <div className='logo'>
+        <Link to="/">eCommerce</Link>
+       </div>
+     <div>
+     <ul><li><Link to="/">Products</Link></li></ul>
+     </div>
+     
+    </div>
+    <div className='right'>
+        right
+    </div>
+     
   </nav>
   )
 }
