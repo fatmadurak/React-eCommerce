c80ccacc0269e893ff3c748e617cde95077bbf00
fatmadurak <fatmabadillidurak@gmail.com>
2023-02-05 20:32:28 +0300

    commited

--- client/src/Pages/ProductDetails/index.js
+++ client/src/Pages/ProductDetails/index.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery } from 'react-query'
 import {fetchProduct} from "../../Api"
-import { Button, Text } from '@chakra-ui/react'
+import { Button, Text,Box} from '@chakra-ui/react'
 import moment from 'moment'
+import ImageGallery from 'react-image-gallery';
+
 function ProductDetails() {
     const {product_id}=useParams();
     const { isLoading, error, data } = useQuery(["products",product_id], () =>fetchProduct(product_id))
     if (isLoading) return 'Loading...'
  
     if (error) return 'An error has occurred: ' + error.message
+
+    const images =data.images.map((url)=>({original:url}))
 
   return (
    
@@ -18,6 +22,12 @@ function ProductDetails() {
         <Text mt={3}>{data.title}</Text>
        <Text> {moment(data.createdAt).format("DD/MM/YYYY")}</Text>
         <p>{data.description}</p>
+
+       <Box mt={10} height={1000} width={1000}>
+
+       <ImageGallery items={images}/>
+
+       </Box>
     </div>
     
     
