050ba425aacf016e2db90d88fe9b012fc5c98af4
fatmadurak <fatmabadillidurak@gmail.com>
2023-02-18 22:04:12 +0300

    committed

--- client/src/Pages/Basket/index.js
+++ client/src/Pages/Basket/index.js
@@ -1,8 +1,54 @@
 import React from 'react'
-
+import {useBasket} from "../../contexts/BasketContext"
+import {Alert,Button,Image} from "@chakra-ui/react"
+import {Link} from  "react-router-dom"
 function Basket() {
+
+  const {items}=useBasket();
   return (
-    <div>Basket</div>
+    <div>
+
+     {items.length <1 && (
+
+      <Alert status='warning'> You have not any items in your basket</Alert>
+
+
+
+     )}
+
+     {
+       items.length >0 && (
+
+         <>
+         
+
+            <ul>
+            {
+              items.map((item)=>(
+
+                <li key={item.id}>
+
+                  <Link to={`/product/${item.id}`}>{item.title}-{item.price} TL</Link>
+                  <Image htmlWidth={300} borderRadius={'2xl'}   src={item.images[0]} alt="basket item"/>
+                  <Button colorScheme="pink" mt={3}> Remove from basket</Button>
+                </li>
+              ))
+             
+
+            }
+            </ul>
+         
+         
+         
+         </>
+
+       )
+     }
+
+
+
+
+    </div>
   )
 }
 
