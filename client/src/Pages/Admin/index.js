d68dff2068f3b9fa1afa0b2831d46d723a4b6f46
fatmadurak <fatmabadillidurak@gmail.com>
2023-02-20 12:25:54 +0300

    committed

--- client/src/Pages/Admin/index.js
+++ client/src/Pages/Admin/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import "./Styles.css"
 
 function Admin() {
   return (
-    <div>Admin</div>
+    <div>
+
+     <nav>
+      <ul className='admin-menu'>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Products</Link>
+        </li>
+        <li>
+          <Link to="/">Orders</Link>
+        </li>
+      </ul>
+     </nav>
+
+
+    </div>
   )
 }
 
