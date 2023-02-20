a34fe12250185771122f1712690f9c744331503c
fatmadurak <fatmabadillidurak@gmail.com>
2023-02-20 13:51:50 +0300

    committed

--- client/src/Pages/Admin/index.js
+++ client/src/Pages/Admin/index.js
@@ -3,6 +3,8 @@ import {NavLink, Routes,Route} from "react-router-dom"
 import "./Styles.css"
 import {Box,Button} from"@chakra-ui/react"
 import Home from './Home'
+import Products from './Products'
+import Orders from './Orders'
 
 function Admin() {
 
@@ -14,20 +16,22 @@ function Admin() {
      <nav>
       <ul className='admin-menu'>
         <li>
-          <NavLink to="home"><Button color="white" backgroundColor="#c0b9">Home</Button></NavLink>
+          <NavLink to="/admin"><Button color="white" backgroundColor="#c0b9">Home</Button></NavLink>
         </li>
         <li>
-         <NavLink to="/admin/home"><Button color="white" backgroundColor="#c0b9">Products</Button></NavLink>
+         <NavLink to="/admin/products"><Button color="white" backgroundColor="#c0b9">Products</Button></NavLink>
         </li>
         <li>
-        <NavLink to="/admin/home"><Button color="white" backgroundColor="#c0b9">Orders</Button></NavLink>
+        <NavLink to="/admin/orders"><Button color="white" backgroundColor="#c0b9">Orders</Button></NavLink>
         </li>
       </ul>
      </nav>
 
-     <Box mt="20">
+     <Box mt="50">
             <Routes>
-                <Route  path="admin/home" exact element={<Home/>} />
+                <Route  path="/" exact element={<Home/>} />
+                <Route path='products' element={<Products/>}/>
+                <Route path='orders' element={<Orders/>}/>
                
             </Routes>
         </Box>
